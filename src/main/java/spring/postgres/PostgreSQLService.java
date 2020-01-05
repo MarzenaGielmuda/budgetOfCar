20f3fa284e908382f1d7210bc4e0c0b2ce35583a
Marzena Gielmuda <m.gielmuda@wp.pl>
2020-01-05 17:45:58 +0100

    four commit- service option - without garbage code

--- src/main/java/spring/postgres/PostgreSQLService.java
+++ src/main/java/spring/postgres/PostgreSQLService.java
@@ -1,16 +1,8 @@
 package spring.postgres;
 
-import com.sun.corba.se.pept.transport.ConnectionCache;
-import org.w3c.dom.ls.LSOutput;
-import spring.type.Book;
 import spring.type.Service;
 
-import java.text.DateFormat;
 import java.sql.*;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +27,7 @@ public class PostgreSQLService  {
     private final String VALUE = "value";
 //    private final String  DATE = "date";
     private final String DESCRIPTION = "description";
+    private final String LastID = "LastID";
 
 
 
@@ -73,8 +66,6 @@ public class PostgreSQLService  {
         try {
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlSelectAllServices);
-
-
 
 
             while (resultSet.next()){
@@ -122,11 +113,11 @@ public class PostgreSQLService  {
 
         PostgreSQLService postgre = new PostgreSQLService();
 
-//        int id = postgre.getIdSql()+1;
+        int id = postgre.getIdService()+1;
         try {
             preparedStatement = connection.prepareStatement(sqlInsertService);
 
-            preparedStatement.setLong(1,service.getId());
+            preparedStatement.setLong(1,id);
             preparedStatement.setDouble(2,service.getValue());
             preparedStatement.setString(3, service.getDescription());
 
@@ -142,13 +133,101 @@ public class PostgreSQLService  {
 
     }
 
-    public int getIdSql(){
+//    postgresServiceStorage.getService(service_id);
+    public  Service getService(String service_id){
+        final  String sqlSelectService = "SELECT * FROM service WHERE id = ?;";
 
-        final String sqlInsertService = " SELECT MAX(ID) AS LastID FROM service";
         Connection connection = initializeDataBaseConnection();
         PreparedStatement preparedStatement = null;
-        return Integer.parseInt(sqlInsertService);
+        int i= Integer.parseInt(service_id);
+        try {
+            preparedStatement = connection.prepareStatement(sqlSelectService);
+            preparedStatement.setLong(1, i);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()){
+                Service service = new Service();
+                service.setId(resultSet.getLong(ID_SERVICE));
+                service.setValue(resultSet.getDouble(VALUE));
+                service.setDescription(resultSet.getString(DESCRIPTION));
+
+                return service;
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Error during invoke SQL query :\n" + e.getMessage());
+            throw new RuntimeException("Error during invoke SQL query");
+        }finally {
+            closeDataBaseResources(connection,preparedStatement);
+        }
+
+        return null;
     }
+
+
+    public void saveEditService(Service service){
+        //
+//        UPDATE Customers
+//        SET ContactName='Alfred Schmidt', City='Frankfurt'
+//        WHERE CustomerID=1;
+        long a = service.getId();
+        final String sqlInsertService = "UPDATE service SET value, description WHERE id="+ a +
+                "VALUES" +
+                "   (?,?);";
+        Connection connection = initializeDataBaseConnection();
+        PreparedStatement preparedStatement = null;
+
+        PostgreSQLService postgre = new PostgreSQLService();
+
+        try {
+            preparedStatement = connection.prepareStatement(sqlInsertService);
+
+            preparedStatement.setDouble(1,service.getValue());
+            preparedStatement.setString(2, service.getDescription());
+
+            preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            System.err.println("Error during invoke SQL query :\n" + e.getMessage());
+            throw new RuntimeException("Error during invoke SQL query");
+        }finally {
+            closeDataBaseResources(connection, preparedStatement);
+        }
+
+
+    }
+    public int getIdService() {
+        final  String sqlSeletService = "SELECT MAX(ID) AS LastID FROM service";
+        Connection connection = initializeDataBaseConnection();
+        Statement statement = null;
+        List<Service> services = new ArrayList<>();
+        int x=0;
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlSeletService);
+
+            while (resultSet.next()){
+
+
+                 x= resultSet.getInt(LastID);
+
+            }
+        } catch (SQLException e) {
+
+            System.err.println("Error during invoke SQL query: \n" + e.getMessage());
+            throw new  RuntimeException("Error during invoke SQL query");
+        }
+        finally {
+            closeDataBaseResources(connection,statement);
+        }
+
+//        Service service= new Service();
+//        service= services.get(0);
+//        int i= (int) service.getId();
+        return  x;
+    }
+
 
 
     private Connection initializeDataBaseConnection(){
@@ -177,65 +256,5 @@ public class PostgreSQLService  {
             throw new RuntimeException("Error during closing database resources");
         }
     }
-
-//    @Override
-//    public Book getBook(long id) {
-//
-//        final  String sqlSeletBook = "SELECT * FROM books WHERE book_id = ?;";
-////        final  String sqlSeletBook = "SELECT * FROM books WHERE book_id = ;"+ id;
-//
-//        Connection connection = initializeDataBaseConnection();
-//        PreparedStatement preparedStatement = null;
-//
-//        try {
-//            preparedStatement = connection.prepareStatement(sqlSeletBook);
-//            preparedStatement.setLong(1, id);
-//            ResultSet resultSet = preparedStatement.executeQuery();
-//
-//            if (resultSet.next()){
-//                Book book = new Book();
-//                book.setId(resultSet.getInt(ID));
-//                book.setTitle(resultSet.getString(TITLE));
-//                book.setAuthor(resultSet.getString(AUTHOR));
-//                book.setPagesSum(resultSet.getInt(PAGE_SUM));
-//                book.setYearOfPublished(resultSet.getInt(YEAR_OF_PUBLISHED));
-//                book.setPublishingHouse(resultSet.getString(PUBLISHING_HOUSE));
-//
-//               return book;
-//
-//            }
-//        } catch (SQLException e) {
-//            System.err.println("Error during invoke SQL query :\n" + e.getMessage());
-//            throw new RuntimeException("Error during invoke SQL query");
-//        }finally {
-//            closeDataBaseResources(connection,preparedStatement);
-//        }
-//        return null;
-//    }
-//
-//
-
-//
-//    @Override
-//    public void removeBook(long bookIdToDelete) {
-//
-//        final  String sqlSelectBook = "DELETE FROM books WHERE  book_id= ?;";
-//
-//        Connection connection = initializeDataBaseConnection();
-//        PreparedStatement preparedStatement = null;
-//
-//        try {
-//            preparedStatement = connection.prepareStatement(sqlSelectBook);
-//            preparedStatement.setLong(1, bookIdToDelete);
-//            preparedStatement.executeUpdate();
-//        } catch (SQLException e) {
-//            System.err.println("Error during invoke SQL query :\n" + e.getMessage());
-//            throw new RuntimeException("Error during invoke SQL query");
-//        }finally {
-//            closeDataBaseResources(connection, preparedStatement);
-//        }
-//
-//
-//    }
 
 }
--- src/main/java/spring/service/ServiceController.java
+++ src/main/java/spring/service/ServiceController.java
@@ -1,60 +1,35 @@
 package spring.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import spring.postgres.PostgreSQLService;
 
-import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import spring.type.Service;
 
-import static java.lang.Long.getLong;
 import static java.lang.Long.parseLong;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Controller
 public class ServiceController {
 
-//    public ServiceController() {
-//        addToList(new Emp( "Janek", 120000, "Radom"));
-//        addToList(new Emp("Zosia", 9000, "Makowiec"));
-//        addToList(new Emp("Marek", 10000, "Warszawa"));
-//        addToList(new Emp("Krysytna", 13000, "Ryzowice"));
-//    }
 
-    //    List<Emp> list = new ArrayList<>();
+
+
     List<Service> listService = new ArrayList<>();
 
     PostgreSQLService postgresServiceStorage = new PostgreSQLService();
-
-
-//    public void addToList(Emp emp) {
-//
-//       emp.setId(list.size()+1);
-//        list.add(emp);
-//
-//    }
 
     @RequestMapping("/")
     public String indexGet() {
 
         return "budget/serviceIndex";
-//        return "emp/index";
     }
 
-    //    @RequestMapping("/serviceIndex")
-//    public ModelAndView viewService(Model model) {
-//
-//        listService = postgresBookStorage.getAllServices();
-//
-//
-//        return new ModelAndView("budget/serviceGetAll", "list", list);
-//    }
     @RequestMapping("/serviceGetAll")
     public ModelAndView serviceGetAll(Model model) {
 
@@ -80,13 +55,29 @@ public class ServiceController {
             postgresServiceStorage.addService(service);
 
         } else {
-            Service service1 = getIdService((int) service.getId());
-            service1.setValue(service.getValue());
-            service1.setDescription(service.getDescription());
+            postgresServiceStorage.saveEditService(service);
 
         }
             return new ModelAndView("redirect:/serviceGetAll");
 
+    }
+//
+
+//
+        @RequestMapping(value = "/edit_service")
+
+//        public ModelAndView showEdit1(Model model) {
+////
+//            return new ModelAndView("budget/serviceAdd", "service", new Service());
+//        }
+
+
+    public ModelAndView editMode(@RequestParam(value = "id") String id){
+
+       Service service = postgresServiceStorage.getService(id);
+
+
+        return new ModelAndView("budget/serviceAdd", "service", service );
     }
 
 
@@ -111,102 +102,8 @@ public class ServiceController {
     }
 
 
-
-//    @RequestMapping(value = "/save_emp")
-//    public ModelAndView save(@ModelAttribute(value = "emp") Emp emp) {
-//
-//        if(emp.getId()<1){
-//            System.out.println("is add");
-//            emp.setId(list.size()+1);
-//            list.add(emp);
-//
-//        }else{
-//            Emp emp1= getIdEmply(emp.getId());
-//            emp1.setName(emp.getName());
-//            emp1.setDesignation(emp.getDesignation());
-//            emp1.setSalary(emp.getSalary());
-//
-//          //  SendEmailEmp.sendEmail(emp.getEmail());
-//
-//        }
-//
-//        return new ModelAndView("redirect:/viewemp2");
-//    }
-
     private Service getIdService(@RequestParam int id) {
         listService.stream().filter(f -> f.getId() == id).forEach(System.out::println);
         return listService.stream().filter(f -> f.getId() == id).findFirst().get();
     }
-//        return new ModelAndView("budget/serviceAdd", "service", service );
-////        return new ModelAndView("emp/edit_emp", "list", list);
-//    }
-//
-//    private Service getIdService(@RequestParam int id){
-//        listService.stream().filter(f -> f.getId()== id).forEach(System.out::println);
-//        return  listService.stream().filter(f -> f.getId()== id).findFirst().get();
-//    }
-
-
-//    @RequestMapping(value = "/empform", method = RequestMethod.GET)
-//    public ModelAndView showform(Model model) {
-//
-//        return new ModelAndView("emp/empform", "emp", new Emp());
-//    }
-//
-//    @RequestMapping(value = "/save_emp")
-//    public ModelAndView save(@ModelAttribute(value = "emp") Emp emp) {
-//
-//        if(emp.getId()<1){
-//            System.out.println("is add");
-//            emp.setId(list.size()+1);
-//            list.add(emp);
-//
-//        }else{
-//            Emp emp1= getIdEmply(emp.getId());
-//            emp1.setName(emp.getName());
-//            emp1.setDesignation(emp.getDesignation());
-//            emp1.setSalary(emp.getSalary());
-//
-//          //  SendEmailEmp.sendEmail(emp.getEmail());
-//
-//        }
-//
-//        return new ModelAndView("redirect:/viewemp2");
-//    }
-//
-//    @RequestMapping(value = "/edit_emp")
-//    public ModelAndView editMode(@RequestParam(value = "emp_id") String emp_id){
-//
-//        Emp emp= getIdEmply(Integer.parseInt(emp_id));
-//
-//        return new ModelAndView("emp/empform", "emp", emp );
-////        return new ModelAndView("emp/edit_emp", "list", list);
-//    }
-//
-//
-//    @RequestMapping(value = "/delete_emp")
-//    public ModelAndView delete(@RequestParam(value = "emp_id") String emp_id ) {
-//
-//
-//        list.remove(getIdEmply(Integer.parseInt(emp_id)));
-//
-//        return new ModelAndView("redirect:/viewemp2");
-//    }
-//
-//    @RequestMapping("/viewemp")
-//    public ModelAndView viewemp(Model model) {
-//
-//        return new ModelAndView("emp/viewemp", "list", list);
-//    }
-//
-//    @RequestMapping("/viewemp2")
-//    public ModelAndView viewemp2(Model model) {
-//        System.out.println("Witaj Marzena !!");
-//        return new ModelAndView("emp/viewemp2", "list", list);
-//    }
-//
-//    private Emp getIdEmply(@RequestParam int id){
-//        list.stream().filter(f -> f.getId()== id).forEach(System.out::println);
-//        return  list.stream().filter(f -> f.getId()== id).findFirst().get();
-//    }
 }
